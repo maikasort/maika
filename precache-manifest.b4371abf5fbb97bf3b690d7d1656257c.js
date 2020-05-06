4fa4a973ebd4fbc02683204f30db6b99ea0b4cf7
maikaSort <mdiomande0205@gmail.com>
2020-05-05 22:55:18 -0700

    Updates

---
+++ precache-manifest.b4371abf5fbb97bf3b690d7d1656257c.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66c13ff216d2fa56be9c7317f08857b0",
+    "url": "/maika/index.html"
+  },
+  {
+    "revision": "92b9a6e87a3fb051c6c8",
+    "url": "/maika/static/css/main.ea80d788.chunk.css"
+  },
+  {
+    "revision": "53ce83c91609cd294a05",
+    "url": "/maika/static/js/2.e1ddfeb9.chunk.js"
+  },
+  {
+    "revision": "3e30755253c48b562917e8850d8c975b",
+    "url": "/maika/static/js/2.e1ddfeb9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "92b9a6e87a3fb051c6c8",
+    "url": "/maika/static/js/main.c5d03967.chunk.js"
+  },
+  {
+    "revision": "d334aa9222f26e4c1104",
+    "url": "/maika/static/js/runtime-main.b296594e.js"
+  },
+  {
+    "revision": "e8ae12d2ef1b4bc5488f645823842a0a",
+    "url": "/maika/static/media/photo4.e8ae12d2.jpg"
+  },
+  {
+    "revision": "8878ed491c858df69017ed020887ad9e",
+    "url": "/maika/static/media/photo5.8878ed49.jpg"
+  },
+  {
+    "revision": "4511cd5603a1a3c85eaf4fec8ceb363d",
+    "url": "/maika/static/media/video-me.4511cd56.mp4"
+  }
+]);
